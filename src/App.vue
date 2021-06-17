f1c1be03905621f4222c40221771e8d960d07859
Casey Eickhoff <48574582+caseyisonit@users.noreply.github.com>
2021-06-17 13:48:29 -0600

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <nav-bar></nav-bar>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import NavBar from "./components/NavBar.vue";
+
+export default Vue.extend({
+  name: "App",
+  components: {
+    HelloWorld,
+    NavBar,
+  },
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="nav-bar">
+        <img class="logo" alt="Vue logo" src="../assets/logo.png" />
+        <input class="search-input" placeholder="Search" /> 
+        <button class="search-button" role="submit">Go</button>
+    </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+.nav-bar {
+    background-color: black;
+    height: 100vh;
+    width: 300px;
+    position: absolute;
+    left: 0;
+    top: 0;
+}
+
+.logo {
+    height: 75px;
+    padding: 1rem;
+    display: flex;
+    margin: auto;
+}
+
+.search-input {
+    color: white;
+}
+</style>
